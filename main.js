db9c4790f42896c38e2cfa4ffdd54e8c1acbb935
jorovernier <jorovernier@gmail.com>
2023-04-27 11:28:16 -0700

    added pushing for loop

--- main.js
+++ main.js
@@ -12,14 +12,22 @@ backpack.push(foundItem)
 
 backpack.pop()
 
-// console.log(backpack.length)
+let itemCount = backpack.length
+// console.log(itemCount)
 
 backpack.push('great ball', 'antidote', 'revive')
 
-let satchel = backpack.splice(3, 2)
+for(let i = 0; i < 10; i++){
+    backpack.push('great ball')
+}
+
+// let satchel = backpack.splice(3, 2)
+// console.log(satchel)
+
+console.log(backpack)
 
 // for(let i = 0; i < backpack.length; i++){
-//     console.log(`This is the item at the index of ${i}:`, backpack[i])
+//     console.log(backpack[i])
 // }
 
 if(backpack.length >= 3){
@@ -28,31 +36,28 @@ if(backpack.length >= 3){
     }
 } else {
     for(let i = 0; i < backpack.length; i++){
-        console.log(`This is the item at the index of ${i}:`, backpack[i])
+        console.log(backpack[i])
     }
 }
 
-// console.log(backpack)
-// console.log(satchel)
-
 // ---------------------------------------------------------
 
-// let guessMe = 54
+let guessMe = 54
 
-// while (guessMe < 100) {
-//     console.log('------------', guessMe)
-//     if (guessMe % 4 === 0 || guessMe % 5 === 0) {
-//         guessMe += 25
-//         console.log('divisible by 4 or 5, added 25', guessMe)
-//     } else if (guessMe % 3 === 0) {
-//         guessMe -= 27
-//         console.log('divisible by 3, subtracted 27', guessMe)
-//     } else {
-//         guessMe += 3
-//         console.log('added 3', guessMe)
-//     }
-//     guessMe += 22
-//     console.log('added 22', guessMe)
-// }
+while (guessMe < 100) {
+    console.log('------------', guessMe)
+    if (guessMe % 4 === 0 || guessMe % 5 === 0) {
+        guessMe += 25
+        console.log('divisible by 4 or 5, added 25', guessMe)
+    } else if (guessMe % 3 === 0) {
+        guessMe -= 27
+        console.log('divisible by 3, subtracted 27', guessMe)
+    } else {
+        guessMe += 3
+        console.log('added 3', guessMe)
+    }
+    guessMe += 22
+    console.log('added 22', guessMe)
+}
 
-// console.log('final value', guessMe)
+console.log('final value', guessMe)
